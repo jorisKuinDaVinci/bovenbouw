184acf63161904afb8a426e093bdab3dfbd90428
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-02-13 13:42:27 +0100

    C# bestanden toegevoegt
    Train opdracht en Pokemon opdracht begin.

---
+++ Openbaar_Vervoer/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Openbaar_Vervoer
+{
+    class Train
+    {
+        public int aantalWielen;
+        public int aantalPlaatsen;
+        public string conducteur;
+
+        public Train(int wielAantal, int plaatsen, string conducteursnaam)
+        {
+            this.aantalWielen = wielAantal;
+            this.aantalPlaatsen = plaatsen;
+            this.conducteur = conducteursnaam;
+        }
+
+        public int rijden()
+        {
+            return 130;
+        }
+
+        public int remmen()
+        {
+            return 0;
+        }
+    }  
+
+    class Program {
+        public static void Main(string[] args)
+        {
+            Train eigenTrein = new Train(7, 30, "Eugene");
+            Console.WriteLine(eigenTrein.aantalWielen);
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Pokemon_Battle_Simulator_joris/Pokemon_Battle_Simulator_joris/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace PokemonBattleSimulator
+{
+    // Charmander class
+    class Charmander
+    {
+        public string Nickname;
+        public string Strength = "Fire";
+        public string Weakness = "Water";
+
+        // Constructor
+        public Charmander(string nickname)
+        {
+            Nickname = nickname;
+        }
+
+        // Method for battle cry
+        public void BattleCry()
+        {
+            Console.WriteLine(Nickname + "! " + Nickname + "!");
+        }
+
+        // Method to rename Charmander
+        public void Rename(string newName)
+        {
+            if (newName != "")
+            {
+                Nickname = newName;
+            }
+            else
+            {
+                Console.WriteLine("Invalid name. Try again.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // 1. The player starts the game.
+            Console.WriteLine("Welcome to the Pokémon battle simulator!");
+
+            // 2. The player gives a name to a Charmander.
+            Console.Write("Enter a name for your Charmander: ");
+            string name = Console.ReadLine();
+            Charmander charmander = new Charmander(name);
+
+            while (true)
+            {
+                // 3. The Charmander does its battle cry ten times.
+                for (int i = 0; i < 10; i++)
+                {
+                    charmander.BattleCry();
+                }
+
+                // 4. The player can give a new name to the same Charmander.
+                Console.Write("Enter a new name for your Charmander (or type 'quit' to exit): ");
+                string newName = Console.ReadLine();
+
+                if (newName.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                charmander.Rename(newName);
+            }
+
+            Console.WriteLine("Game over. Thanks for playing!");
+        }
+    }
+}
