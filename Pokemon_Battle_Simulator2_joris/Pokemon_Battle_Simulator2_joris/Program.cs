52e5b67c84601d0064171daa5e83286702e529cc
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-02-14 10:07:21 +0100

    pokemon opdracht 2 bestand aangemaakt

---
+++ Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pokemon_Battle_Simulator2_joris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pokemon_Battle_Simulator2_joris")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pokemon_Battle_Simulator2_joris")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3328a83-7961-44fe-956d-fa5cc8d71c0a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
