fe3934b7fc390839d868a6efb589e1f2208607a3
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-02-14 10:47:44 +0100

    charmander class aangemaakt

--- Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Program.cs
+++ Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Program.cs
@@ -2,10 +2,22 @@
 
 namespace Pokemon_Battle_Simulator2_joris
 {
-    class Program
+    public class Charmander
     {
-        static void Main(string[] args)
+        public bool field = false;
+        public bool constructor = true;
+        public string nickname;
+        public string strength = "fire";
+        public string weakness = "water";
+
+        public Charmander(string nickname)
         {
+            this.nickname = nickname;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Charmander " + nickname + ": Strength = " + strength + ", Weakness = " + weakness);
         }
     }
 }
