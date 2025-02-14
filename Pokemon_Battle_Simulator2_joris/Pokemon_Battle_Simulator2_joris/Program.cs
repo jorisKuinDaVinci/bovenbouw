64f42f7acd1d34e524ec360cf0a627826f59d93e
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-02-14 11:49:54 +0100

    pokeball class aangemaakt

--- Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Program.cs
+++ Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Program.cs
@@ -20,4 +20,40 @@ namespace Pokemon_Battle_Simulator2_joris
             Console.WriteLine("Charmander " + nickname + ": Strength = " + strength + ", Weakness = " + weakness);
         }
     }
+    public class Pokeball
+    {
+        public bool inBall = true;
+        public Charmander containedPokemon;
+
+        public Pokeball(Charmander pokemon)
+        {
+            containedPokemon = pokemon;
+        }
+
+        public void Throw()
+        {
+            if (!inBall)
+            {
+                Console.WriteLine("The pokeball is already open!");
+            }
+            else
+            {
+                Console.WriteLine("You threw the pokeball! A " + containedPokemon.GetType().Name + " appears!");
+                inBall = false;
+            }
+        }
+
+        public void Return()
+        {
+            if (!inBall)
+            {
+                Console.WriteLine("The Charmander returns to its Pokéball. The lid closes up.");
+                inBall = true;
+            }
+            else
+            {
+                Console.WriteLine("The Pokéball is already closed.");
+            }
+        }
+    }
 }
