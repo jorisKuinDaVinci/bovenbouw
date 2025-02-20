52bf69c8f92e5ae579484af89ab6e4af46328816
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-02-20 13:27:31 +0100

    charmander en pokeball in losse bestanden

---
+++ Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Charmander.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pokemon_Battle_Simulator2_joris
+{
+    public class Charmander
+    {
+        public string nickname;
+        public string strength = "fire";
+        public string weakness = "water";
+
+        public Charmander(string nickname)
+        {
+            this.nickname = nickname;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Charmander " + nickname + ": Strength = " + strength + ", Weakness = " + weakness);
+        }
+
+        // Battle cry
+        public string BattleCry()
+        {
+            return nickname + "! " + nickname + "!";
+        }
+    }
+}
---
+++ Pokemon_Battle_Simulator2_joris/Pokemon_Battle_Simulator2_joris/Pokeball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pokemon_Battle_Simulator2_joris
+{
+    public class Pokeball
+    {
+        public bool inBall = true;
+        public Charmander containedPokemon;
+
+        public Pokeball(Charmander pokemon)
+        {
+            containedPokemon = pokemon;
+        }
+
+        public void Throw()
+        {
+            if (!inBall)
+            {
+                Console.WriteLine("The Pokeball is already open!");
+            }
+            else
+            {
+                Console.WriteLine("You threw the Pokeball! A " + containedPokemon.nickname + " appears!");
+                Console.WriteLine(containedPokemon.BattleCry()); // Charmander battle cry
+                inBall = false;
+            }
+        }
+
+        public void Return()
+        {
+            if (!inBall)
+            {
+                Console.WriteLine("The " + containedPokemon.nickname + " returns to its Pokeball. The lid closes up.");
+                inBall = true;
+            }
+            else
+            {
+                Console.WriteLine("The Pokeball is already closed.");
+            }
+        }
+    }
+}
