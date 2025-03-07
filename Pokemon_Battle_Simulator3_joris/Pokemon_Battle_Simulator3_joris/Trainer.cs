c9d51eb9062969d398f33b92a62e54b34983bf53
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-03-07 09:26:10 +0100

    pokemon opdracht 3 start

---
+++ Pokemon_Battle_Simulator3_joris/Pokemon_Battle_Simulator3_joris/Trainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokemon_Battle_Simulator3_joris
+{
+    public class Trainer
+    {
+        public string name;
+        public List<Pokeball> belt = new List<Pokeball>(6);
+
+        public Trainer(string name)
+        {
+            this.name = name;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                belt.Add(new Pokeball(new Charmander("Charmander" + i)));
+            }
+        }
+
+        public void ThrowPokeball(int index)
+        {
+            if (index < 0 || index >= belt.Count)
+            {
+                Console.WriteLine("Invalid Pokeball index.");
+                return;
+            }
+
+            Console.WriteLine(name + " throws a Pokeball!");
+            belt[index].Throw();
+        }
+
+        public void ReturnPokemon(int index)
+        {
+            if (index < 0 || index >= belt.Count)
+            {
+                Console.WriteLine("Invalid Pokeball index.");
+                return;
+            }
+
+            Console.WriteLine(name + " returns the Pokémon.");
+            belt[index].Return();
+        }
+    }
+}
