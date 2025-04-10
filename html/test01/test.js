92405051fc3441e39b63050f15a7d8b8c85687c2
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-04-10 09:48:22 +0200

    les gevolgd

--- html/test01/test.js
+++ html/test01/test.js
@@ -1,14 +1,22 @@
 var button = document.getElementById("createButton");
-console.log(button);
+var nameList = ["Piet", "Henk", "Pablo", "Escobar", "Bob", "Corbijn"]
 
-button.addEventListener("click", createAnotherElement)
+button.addEventListener("click", createElementsFromList)
 
-function createAnotherElement() {
-    var newButton = document.createElement("button")
-    newButton.setAttribute("type", "type");
-    newButton.setAttribute("value", "click me for an alert");
-    newButton.addEventListener("click", function() {
-        alert("Button clicked!");
-    });
-    document.boby.appendChild(newButton); 
+function createElementsFromList() {
+        for (let i = 0; i < nameList.length; i++) {
+            let newButton = document.createElement("input")
+            newButton.setAttribute("type", "button");
+            newButton.setAttribute("value", nameList[i]);
+            newButton.classList.add("button")
+            newButton.addEventListener("click", function(){ 
+                andereFunction(nameList[i]);
+            });
+            document.boby.appendChild(newButton);
+        }
+
+}
+
+function andereFunction(name) {
+    alert(name);
 }
